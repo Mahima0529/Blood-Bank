343219e5c347b2a02ca200bf8473a25bb6f9d00d
Mahima0529 <mahimaptel@gmail.com>
2025-05-19 14:36:43 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+//rest object
+const app= express();
+
+//routes
+//1 test route
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "Welcome to Blood Bank",
+    });
+});
+
+//port
+const PORT= 8080;
+
+//listen
+app.listen(PORT,()=>{
+    console.log("Node server is running");
+});
+
